604a8e99fb05505f88e8d164d2706a23abdcbe5e
Samuele Bistoletti <s.bistoletti@18months.it>
2015-06-03 16:08:36 +0200

    Added Find endpoint

--- lib/tmdb/find.rb
+++ lib/tmdb/find.rb
@@ -1,5 +1,45 @@
 module Tmdb
-  class Find
+  class Find < Struct
+
+    def self.movie(id, filters={})
+      result = Resource.new("/find/#{id}", filters).run
+
+      result['movie_results'].map do |entry|
+        self.new(entry)
+      end
+    end
+
+    def self.people(id, filters={})
+      result = Resource.new("/find/#{id}", filters).run
+
+      result['person_results'].map do |entry|
+        self.new(entry)
+      end
+    end
+
+    def self.tv_serie(id, filters={})
+      result = Resource.new("/find/#{id}", filters).run
+
+      result['tv_results'].map do |entry|
+        self.new(entry)
+      end
+    end
+
+    def self.tv_season(id, filters={})
+      result = Resource.new("/find/#{id}", filters).run
+
+      result['tv_episode_results'].map do |entry|
+        self.new(entry)
+      end
+    end
+
+    def self.tv_episode(id, filters={})
+      result = Resource.new("/find/#{id}", filters).run
+
+      result['tv_season_results'].map do |entry|
+        self.new(entry)
+      end
+    end
 
   end
 end
