86c4cdafccd8c2d146ba506bd469a46ba3f0b2bd
Samuele Bistoletti <s.bistoletti@18months.it>
2015-06-03 14:09:13 +0200

    Added Certification endpoint

--- lib/tmdb/certification.rb
+++ lib/tmdb/certification.rb
@@ -1,11 +1,22 @@
 module Tmdb
   class Certification < Struct
 
-    def self.list(filters={})
+    def self.movie_list(filters={})
       output = {}
       result = Resource.new('/certification/movie/list', filters).run
 
-      result['certifications'].map do |nation,certification_list|
+      result['certifications'].map do |nation, certification_list|
+        output[nation.to_sym] = certification_list.map { |cert| self.new(cert) }
+      end
+
+      output
+    end
+
+    def self.tv_list(filters={})
+      output = {}
+      result = Resource.new('/certification/tv/list', filters).run
+
+      result['certifications'].map do |nation, certification_list|
         output[nation.to_sym] = certification_list.map { |cert| self.new(cert) }
       end
 
