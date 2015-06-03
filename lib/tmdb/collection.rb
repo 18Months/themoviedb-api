a8592ba737a9a2531c5b6b7d9bddcd455fa25321
Samuele Bistoletti <s.bistoletti@18months.it>
2015-06-03 14:36:19 +0200

    Added Collection endpoint

--- lib/tmdb/collection.rb
+++ lib/tmdb/collection.rb
@@ -1,4 +1,15 @@
 module Tmdb
   class Collection < Struct
+
+    def self.detail(id, filters={})
+      result = Resource.new("/collection/#{id}", filters).run
+      self.new(result)
+    end
+
+    def self.images(id, filters={})
+      result = Resource.new("/collection/#{id}/images", filters).run
+      self.new(result)
+    end
+
   end
 end
