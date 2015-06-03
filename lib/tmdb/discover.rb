be98fb26ba507d7a7554a203df6dc0bfa00916e3
Samuele Bistoletti <s.bistoletti@18months.it>
2015-06-03 15:50:43 +0200

    Added Discover endpoint

--- lib/tmdb/discover.rb
+++ lib/tmdb/discover.rb
@@ -1,4 +1,15 @@
 module Tmdb
   class Discover < Struct
+
+    def self.movie(filters={})
+      result = Resource.new('/discover/movie', filters).run
+      self.new(result)
+    end
+
+    def self.tv(filters={})
+      result = Resource.new('/discover/tv', filters).run
+      self.new(result)
+    end
+
   end
 end
