456d20508590bb6a73cf167c935d0a74560a4c99
Samuele Bistoletti <s.bistoletti@18months.it>
2015-06-03 15:31:57 +0200

    Added Credit endpoint

--- lib/tmdb/credit.rb
+++ lib/tmdb/credit.rb
@@ -1,4 +1,10 @@
 module Tmdb
   class Credit < Struct
+
+    def self.detail(id, filters={})
+      result = Resource.new("/credit/#{id}", filters).run
+      self.new(result)
+    end
+
   end
 end
