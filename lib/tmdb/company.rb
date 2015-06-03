baa8c517fd2e15c37a955513971b4432c8bd1454
Samuele Bistoletti <s.bistoletti@18months.it>
2015-06-03 14:37:39 +0200

    Added Company endpoint

--- lib/tmdb/company.rb
+++ lib/tmdb/company.rb
@@ -1,4 +1,15 @@
 module Tmdb
   class Company < Struct
+
+    def self.detail(id, filters={})
+      result = Resource.new("/company/#{id}", filters).run
+      self.new(result)
+    end
+
+    def self.movies(id, filters={})
+      result = Resource.new("/company/#{id}/movies", filters).run
+      self.new(result)
+    end
+
   end
 end
