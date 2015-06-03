a8b9a1de89cd312e39c2a74b9ed7510552fad561
Samuele Bistoletti <s.bistoletti@18months.it>
2015-06-03 14:29:55 +0200

    Added Changes endpoint

--- lib/tmdb/changes.rb
+++ lib/tmdb/changes.rb
@@ -1,4 +1,20 @@
 module Tmdb
   class Changes < Struct
+
+    def self.movie(filters={})
+      result = Resource.new("/movie/changes", filters).run
+      self.new(result)
+    end
+
+    def self.person(filters={})
+      result = Resource.new("/person/changes", filters).run
+      self.new(result)
+    end
+
+    def self.tv(filters={})
+      result = Resource.new("/tv/changes", filters).run
+      self.new(result)
+    end
+
   end
 end
