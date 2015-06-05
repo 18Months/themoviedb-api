ab5b417f2b1b454c24c9bbe7883b1ed1a427f65e
Samuele Bistoletti <s.bistoletti@18months.it>
2015-06-05 11:09:32 +0200

    Added Review endpoint

--- lib/tmdb/review.rb
+++ lib/tmdb/review.rb
@@ -1,4 +1,10 @@
 module Tmdb
   class Review < Struct
+
+    def self.detail(keyword_id, filters={})
+      result = Resource.new("/review/#{keyword_id}", filters).get
+      self.new(result)
+    end
+
   end
 end
