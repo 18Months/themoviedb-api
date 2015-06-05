d7ba53dbf27a1d0649fa863e1b9462fd658ec2b9
Samuele Bistoletti <s.bistoletti@18months.it>
2015-06-05 16:19:19 +0200

    Partial season implementation

--- lib/tmdb/tv/season.rb
+++ lib/tmdb/tv/season.rb
@@ -1,6 +1,26 @@
 module Tmdb
   module Tv
     class Season < Struct
+
+      def self.detail(id, season_number, filters={})
+        result = Resource.new("/tv/#{id}/season/#{season_number}", filters).get
+        season = self.new(result)
+        
+        season.convert_episodes_references!
+
+        season
+      end
+
+      private
+
+      def convert_episodes_references!
+        episodes.each do |episode|
+          episode.crew.map! do |person|
+            Person.new(person.to_h)
+          end
+        end
+      end
+      
     end
   end
 end
