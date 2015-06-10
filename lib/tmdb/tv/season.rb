5594c2014113498d46c6b22e04c54ed07e0fe28e
Samuele Bistoletti <s.bistoletti@18months.it>
2015-06-10 20:51:14 +0200

    Added TV Season endpoint

--- lib/tmdb/tv/season.rb
+++ lib/tmdb/tv/season.rb
@@ -11,6 +11,67 @@ module Tmdb
         season
       end
 
+      def self.changes(id, filters={})
+        result = Resource.new("/tv/season/#{id}/changes", filters).get
+
+        result['items'].map do |entry|
+          Changes.new(entry)
+        end
+      end
+
+      def self.cast(id, season_number, filters={})
+        result = Resource.new("/tv/#{id}/season/#{season_number}/credits", filters).get
+
+        result['cast'].map do |entry|
+          Person.new(entry)
+        end
+      end
+
+      def self.crew(id, season_number, filters={})
+        result = Resource.new("/tv/#{id}/season/#{season_number}/credits", filters).get
+
+        result['crew'].map do |entry|
+          Person.new(entry)
+        end
+      end
+
+      def self.director(id, season_number, filters={})
+        result = Resource.new("/tv/#{id}/season/#{season_number}/credits", filters).get
+
+        result['crew'].select{ |x| x['job'] == 'Director' }.map do |entry|
+          Person.new(entry)
+        end
+      end
+
+      def self.external_ids(id, season_number, filters={})
+        result = Resource.new("/tv/#{id}/season/#{season_number}", filters).get
+        self.new(result)
+      end
+
+      def self.backdrops(id, season_number, filters={})
+        result = Resource.new("/tv/#{id}/season/#{season_number}/images", filters).get
+
+        result['backdrops'].map do |entry|
+          Backdrop.new(entry)
+        end
+      end
+
+      def self.posters(id, season_number, filters={})
+        result = Resource.new("/tv/#{id}/season/#{season_number}/images", filters).get
+
+        result['posters'].map do |entry|
+          Poster.new(entry)
+        end
+      end
+
+      def self.videos(id, season_number, filters={})
+        result = Resource.new("/tv/#{id}/season/#{season_number}/videos", filters).get
+
+        result['results'].map do |entry|
+          Video.new(entry)
+        end
+      end
+
       private
 
       def convert_episodes_references!
@@ -20,7 +81,6 @@ module Tmdb
           end
         end
       end
-      
     end
   end
 end
