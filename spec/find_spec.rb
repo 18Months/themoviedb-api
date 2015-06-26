fd4dc1572830f1f82717656dcf7837af394923db
Samuele Bistoletti <s.bistoletti@18months.it>
2015-06-26 10:42:25 +0200

    Completed tests on Find class

--- spec/find_spec.rb
+++ spec/find_spec.rb
@@ -57,4 +57,49 @@ describe Tmdb::Find do
     end
   end
 
+  context '#tv_serie' do
+    let(:tv_serie) do
+      VCR.use_cassette 'find/tv_serie' do
+        Tmdb::Find.tv_serie('tt3749900', external_source: 'imdb_id')
+      end
+    end
+
+    subject { tv_serie }
+
+    it 'should return an array of Tmdb::TV' do
+      expect(subject).to be_an_instance_of(Array)
+      expect(subject.sample).to be_an_instance_of(Tmdb::TV)
+    end
+  end
+
+  context '#tv_season' do
+    let(:tv_season) do
+      VCR.use_cassette 'find/tv_season' do
+        Tmdb::Find.tv_season('121361', external_source: 'tvdb_id')
+      end
+    end
+
+    subject { tv_season }
+
+    it 'should return an array of Tmdb::TV' do
+      expect(subject).to be_an_instance_of(Array)
+      expect(subject.sample).to be_an_instance_of(Tmdb::TV)
+    end
+  end
+
+  context '#tv_episode' do
+    let(:tv_episode) do
+      VCR.use_cassette 'find/tv_episode' do
+        Tmdb::Find.tv_episode('121361', external_source: 'tvdb_id')
+      end
+    end
+
+    subject { tv_episode }
+
+    it 'should return an array of Tmdb::TV' do
+      expect(subject).to be_an_instance_of(Array)
+      expect(subject.sample).to be_an_instance_of(Tmdb::TV)
+    end
+  end
+
 end
