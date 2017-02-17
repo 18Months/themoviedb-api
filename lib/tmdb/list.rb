8c0ccb5cde005b1c9355cb38a0a53ee746e4b7df
pc <davide.melkior@gmail.com>
2017-02-17 15:04:26 +0100

    added some list class methods

--- lib/tmdb/list.rb
+++ lib/tmdb/list.rb
@@ -1,4 +1,32 @@
+# frozen_string_literal: true
 module Tmdb
-  class List < Struct
-  end
+    class List < Struct
+        def self.details(list_id)
+            Resource.new("/list/#{list_id}").get
+        end
+
+        def self.item_status(list_id, movie_id)
+            Resource.new("/list/#{list_id}/item_status", movie_id: movie_id).get
+        end
+
+        def self.create(name, description, language)
+            @@session_id = Authentication.get_session_id
+            Resource.new('/list', session_id: @@session_id, name: name, description: description, language: language).post
+        end
+
+        def self.add_item(list_id, media_id)
+            @@session_id = Authentication.get_session_id
+            Resource.new("/list/#{list_id}/add_item", session_id: @@session_id, media_id: media_id).post
+        end
+
+        def self.remove_item(list_id, media_id)
+            @@session_id = Authentication.get_session_id
+            Resource.new("/list/#{list_id}/remove_item", session_id: @@session_id, media_id: media_id).post
+        end
+
+        def self.clear(list_id, confirm)
+            @@session_id = Authentication.get_session_id
+            Resource.new("/list/#{list_id}/clear", session_id: @@session_id, confirm: confirm).post
+        end
+    end
 end
