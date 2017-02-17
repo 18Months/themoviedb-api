9a3d0717bd4d8d44dbd1e5afdbbdea0d882f0af5
pc <davide.melkior@gmail.com>
2017-02-17 16:13:50 +0100

    implemented extra parameters support

--- lib/tmdb/account.rb
+++ lib/tmdb/account.rb
@@ -1,70 +1,76 @@
 # frozen_string_literal: true
 module Tmdb
     class Account < Struct
-        def self.details
+        def self.details(filters = {})
             @@session_id = Authentication.get_session_id
-
-            Resource.new('/account', session_id: @@session_id).get
+            filters[:session_id] = @@session_id
+            Resource.new('/account', filters).get
         end
 
-        def self.lists(account_id = '%7Baccount_id%7D')
+        def self.lists(filters = {})
             @@session_id = Authentication.get_session_id
-
-            Resource.new("/account/#{account_id}/lists", session_id: @@session_id).get
+            filters[:session_id] = @@session_id
+            Resource.new('/account/%7Baccount_id%7D/lists', filters).get
         end
 
-        def self.favorite_movies(account_id = '%7Baccount_id%7D')
+        def self.favorite_movies(filters = {})
             @@session_id = Authentication.get_session_id
-
-            Resource.new("/account/#{account_id}/favorite/movies", session_id: @@session_id).get
+            filters[:session_id] = @@session_id
+            Resource.new('/account/%7Baccount_id%7D/favorite/movies', filters).get
         end
 
-        def self.favorite_tv(account_id = '%7Baccount_id%7D')
+        def self.favorite_tv(filters = {})
             @@session_id = Authentication.get_session_id
-
-            Resource.new("/account/#{account_id}/favorite/tv", session_id: @@session_id).get
+            filters[:session_id] = @@session_id
+            Resource.new('/account/%7Baccount_id%7D/favorite/tv', filters).get
         end
 
-        def self.rated_movies(account_id = '%7Baccount_id%7D')
+        def self.rated_movies(filters = {})
             @@session_id = Authentication.get_session_id
-
-            Resource.new("/account/#{account_id}/rated/movies", session_id: @@session_id).get
+            filters[:session_id] = @@session_id
+            Resource.new('/account/%7Baccount_id%7D/rated/movies', filters).get
         end
 
-        def self.rated_tv(account_id = '%7Baccount_id%7D')
+        def self.rated_tv(filters = {})
             @@session_id = Authentication.get_session_id
-
-            Resource.new("/account/#{account_id}/rated/tv", session_id: @@session_id).get
+            filters[:session_id] = @@session_id
+            Resource.new('/account/%7Baccount_id%7D/rated/tv', filters).get
         end
 
-        def self.rated_episodes(account_id = '%7Baccount_id%7D')
+        def self.rated_episodes(filters = {})
             @@session_id = Authentication.get_session_id
-
-            Resource.new("/account/#{account_id}/rated/tv/episodes", session_id: @@session_id).get
+            filters[:session_id] = @@session_id
+            Resource.new('/account/%7Baccount_id%7D/rated/tv/episodes', filters).get
         end
 
-        def self.movies_watchlist(account_id = '%7Baccount_id%7D')
+        def self.movies_watchlist(filters = {})
             @@session_id = Authentication.get_session_id
-
-            Resource.new("/account/#{account_id}/watchlist/movies", session_id: @@session_id).get
+            filters[:session_id] = @@session_id
+            Resource.new('/account/%7Baccount_id%7D/watchlist/movies', filters).get
         end
 
-        def self.tv_watchlist(account_id = '%7Baccount_id%7D')
+        def self.tv_watchlist(filters = {})
             @@session_id = Authentication.get_session_id
-
-            Resource.new("/account/#{account_id}/watchlist/tv", session_id: @@session_id).get
+            filters[:session_id] = @@session_id
+            Resource.new('/account/%7Baccount_id%7D/watchlist/tv', filters).get
         end
 
-        def self.add_to_watchlist(media_type, media_id, watchlist = true, account_id = '%7Baccount_id%7D')
+        def self.add_to_watchlist(media_type, media_id, watchlist = true, filters = {})
             @@session_id = Authentication.get_session_id
-
-            Resource.new("/account/#{account_id}/watchlist", session_id: @@session_id, media_type: media_type, media_id: media_id, watchlist: watchlist).post
+            filters[:session_id] = @@session_id
+            filters[:media_type] = media_type
+            filters[:media_id] = media_id
+            filters[:watchlist] = watchlist
+            Resource.new('/account/%7Baccount_id%7D/watchlist', filters).post
         end
 
-        def self.mark(media_type, media_id, favorite = true, account_id = '%7Baccount_id%7D')
+        def self.mark(media_type, media_id, favorite = true, filters = {})
             @@session_id = Authentication.get_session_id
-
-            Resource.new("/account/#{account_id}/favorite", session_id: @@session_id, media_type: media_type, media_id: media_id, favorite: favorite).post
+            filters[:session_id] = @@session_id
+            filters[:media_type] = media_type
+            filters[:media_id] = media_id
+            filters[:favorite] = favorite
+            Resource.new('/account/%7Baccount_id%7D/favorite', filters).post
         end
     end
 end
